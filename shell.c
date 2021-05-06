88f8715bb84ff3c35778dea50ed67e62302073d6
19parsonsj <79724637+19parsonsj@users.noreply.github.com>
2021-05-05 20:22:13 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/wait.h>
+#include <libgen.h>
 
 #define MAX 256
 #define MAX_VARS 64
@@ -13,6 +15,8 @@
 #define TOKEN_WHITESPACE " \t\n\r"
 
 void dieWithError(char *);
+
+
 
 // Structure for the variables
 struct variable {
@@ -24,7 +28,6 @@ struct variable {
 // PS = varArray[2]
 struct variable varArray[MAX_VARS];
 int countVar = 3;
-
 int numTokens = 0;
 
 // Fgets wrapper
@@ -40,7 +43,7 @@ char **shell_scanner(char *input) {
   int bufsize = TOKEN_BUFSIZE;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token;
-
+  numTokens = 0;
   if (!tokens)
     dieWithError("Malloc allocation error");
  
@@ -49,7 +52,7 @@ char **shell_scanner(char *input) {
     // printf tokens for debugging
     printf("token %d: %s\n", numTokens, token);
     
-    tokens[pos] = token;
+    tokens[numTokens] = token;
     numTokens++;
 
     if (numTokens >= bufsize)
@@ -58,7 +61,7 @@ char **shell_scanner(char *input) {
     token = strtok(NULL, TOKEN_WHITESPACE);
   }
   tokens[numTokens] = NULL;
-
+  printf("zero value: %s\n", tokens[0]);
   // print tokens for DEBUGGING ONLY
   for (int i = 0; i < numTokens; i++)
     printf("Tokens %d: %s\n", numTokens, tokens[i]);
@@ -67,6 +70,7 @@ char **shell_scanner(char *input) {
 
 // Changes the current working directory
 int changeDir(char **args) {
+  printf("in changedir\n");
   if (args[1] == NULL)
     printf("Error: Expected an argument for cd\n");
   else if (numTokens != 2)
@@ -84,19 +88,27 @@ int changeDir(char **args) {
 // Tests if there are any '#' characters. If there are, it tests if it is used
 // properly or if it is a syntax error.
 int isComment(char **args) {
+  printf("we are in isComment\n");
+  printf("zero value: %s\n", args[0]);
   char *p;
   int foundHash = 0;
   //Search the strings for a #
   for (int i = 0; i < numTokens; i++)
-    if (p = strchr(args[i],'#') != NULL)
+  {
+    //if ((p == strchr(args[i],'#')) != NULL)
+    p = strchr(args[i],'#');
+    if (p[0] != '\0')
+      printf("found hash\n");
       foundHash = 1;
-  
+  }
   // If a "#" wasn't found, return 0. If it was found, it should be the very first char
   // in the first token. If it isn't, it's a syntax error
-  if (foundHash == 0)
+ if (foundHash == 0)
     return 0;
+
   else if (args[0][0] == '#')
     return 1;
+
   else {
    printf("Error: Unexpected character \"#\"\n");
    return 1;
@@ -106,8 +118,9 @@ int isComment(char **args) {
 // Test if any tokens are labeled as variables. If they are, repalce that token with
 // their associated variable values. If the variable is not declared, syntax error.
 int fetchVar(char **args) {
+ int foundSigns = 0;
   
-  int foundSigns = 0;
+
   //Search the tokens for any $. If it is found, the next char should be a letter
   // according to variable assignment rules. If it isn't, it's a syntax error.
   for (int j = 0; j < numTokens; j++) {
@@ -121,7 +134,7 @@ int fetchVar(char **args) {
     // variables in a token. It also catches any error and replaces the variables names with
     // their values in **args
     while (doAgain == 1) {
-      if (point = strchr(args[j],'$') != NULL) {
+      if (point == strchr(args[j],'$') != NULL) {
         foundSigns++;
         char varName[MAX];
         int i = 0;
@@ -179,7 +192,8 @@ int fetchVar(char **args) {
         // token should replace the old one in **args.
         if (*point == '\0') {
           doAgain = 0;
-          args[j] = newToken
+          args[j] = newToken;
+          args[j] = newToken;
         }
         else
           args[j] = point;
@@ -195,6 +209,8 @@ int fetchVar(char **args) {
   return 1;
 }
 
+
+
 // If the first token is alphanumerical with its first character being a letter, AND the second
 // token is "=", then set variable. Note that if "variable = value" has no spaces, (e.g.
 // "variable=value") then it is a syntax error for an unknown command.
@@ -202,56 +218,58 @@ int fetchVar(char **args) {
 // Note that shell_execute tests if there are 3 arguments and the second argument is '=' as this
 // would imply it is a setVar command. Otherwise, it will be labeled as an unknown command
 int setVar(char **args) {
+  printf("we are in setVar");
   if (isalpha(*args[0]) == 0) {
     printf("Error: Invalid variable assignment. First character must be numerical.\n");
     return 1;
   }
   else if (strcmp(args[0], "CWD") == 0) {
     printf("Error: Cannot change variable \"CWD\"\n");
-    returns 1;
+    return 1;
   }
   while (*args[0] != '\0') {
     if (isalnum(*args[0]) == 0) {
       printf("Error: Invalid variable assignment. Variable can only be composed of alpha-numerical characters.\n");
       return 1;
     }
-    *args[0]++;
+    args[0]++;
   }
 
   // Append a '$' to the beginning of the new variable name to simplify searching for variables.
-  char *ch = "$"
+  char *ch = "$";
   char *newName = strcat(ch, args[0]);
- 
-  // Make sure this variable has not been declared before. If it has overwrite the value. If it hasn't create a new one.
+
+   // Make sure this variable has not been declared before. If it has overwrite the value. If it hasn't create a new one.
   int isNewVar = 0;
   int overwriteVal = 0;
  
-  for (int i = 0; i < count; i++) {
+  for (int i = 0; i < countVar; i++) {
     if (varArray[i].name == newName) {
       isNewVar = 1;
       overwriteVal = i;
     }
   }
- 
-  // There is no need to overwrite the name if the variable has been declared before. Simply overwrite the value.
-  if (isNewVar == 1;) {
+   // There is no need to overwrite the name if the variable has been declared before. Simply overwrite the value.
+  if (isNewVar == 1) {
     varArray[overwriteVal].value = args[2];
     return 1;
   }
-  else {
-    varArray[count].name = newName; 
-    varArray[count].value = args[2];
-    count++;
-    return 1;
-  }
+ else {
+  varArray[countVar].name = newName; 
+  varArray[countVar].value = args[2];
+  countVar++;
+  return 1;
+ }
 }
 
 int listVar(char **args) {
+  printf("In listVar\n");
   printf("Variable List: \n");
-  for (int i = 0; i < count; i++)
-    printf("Name: %p\t Value: %p\n", varArray[i].name, varArray[i].value);
+  for (int i = 0; i < countVar; i++)
+    printf("Name: %s\t Value: %s\n", varArray[i].name, varArray[i].value);
   return 1;
 }
+
 
 // Remeber it is an error to unset a built in variable
 int unsetVar(char **args) {
@@ -259,18 +277,18 @@ int unsetVar(char **args) {
       printf("Error: cannot unset defined variables.");
   int varFound = 0;
   // Find the variable desired to be removed
-    for (int i = 0; i < count-1; i++){
+    for (int i = 0; i < countVar-1; i++){
       if (varArray[i].name == args[1]) {
         // Once found, replace elements in the array to simulate the element being removed
         varFound = 1;
-        for (i; i < count; i++) {
-          varArray[i] = varArray[i+1];
+        for (int j = i; j < countVar; j++) {
+          varArray[j] = varArray[j+1];
         }
       }
     }
   if (varFound == 1) {
     // The element has been found and deleted, now decrement count to dispose of the excess variable.
-    count--;
+    countVar--;
     return 1;
   }
   else { // Else the element wasn't declared, which is an error.
@@ -279,8 +297,9 @@ int unsetVar(char **args) {
   }
 }
 
+
 //execute program function
-int execute_program(char **args) {
+
  // !
     //fork 
       //after forking, parent should wait for command it has just 
@@ -293,50 +312,111 @@ int execute_program(char **args) {
           //and re-associate the descriptor number(s) with the newly opened file(s). (look at man for dup2) 
           //then can execve
 
-    //execve new process to execute given file
+  //EXECUTE PROGRAM FUNCTION
+  int execute_program(char **args) {
+  printf("in !\n");
+  pid_t pid, wpid;
+	int fd0, fd1; 
+
+  //fork
+	if((pid=fork())==-1){
+		printf("Child process could not be created\n");
+		return 1;
+	}
+  //child
+	if(pid==0){
+		// check to see if infrom or outto could be tokens
+		if (sizeof(args) > 2){
+      //check to see if there is an infrom token
+      if ((strcmp(args[2], "infrom:") == 0) || ((strcmp(args[4], "infrom:")) == 0)){
+			// We open (create) the file from infrom: _____
+			fd0 = open(args[3]); 
+			dup2(fd0, STDOUT_FILENO); 
+			close(fd0);
+      //re associate descriptor numbers
+     
+      }
+      //check if there is outto by itself or with infrom 
+      if (args[4] == "outto:") == 0) || args[2] == "outto:") {
+        //open
+        fd1 = open(args[5], 0644);
+        dup2(fd1, STDOUT_FILENO);
+        //close
+        close(fd1);
+        //re associate descriptor numbers
+       
+
+      }
+    
+    execvp(args[1], args);
+		
+		}
+  }
+    //parent
+    else {
+      //wait for child to finish before prompting for next command
+      wpid = waitpid(pid,NULL,0);
+
+		if (execvp(args[1],args) == 0){
+			printf("Error: could not recognise use of !");
+			kill(getpid(),SIGINT);
+		}	
+    return 1;	 
+	}
 
 }
 
-int exit(char **args) {
+
+//works
+int exit_shell() {
   //exit with status zero
-  printf("Exiting shell: exit status zero.");
+  printf("Exiting shell: exit status zero.\n");
   return 0;
 }
 
 
 
 int shell_execute(char **args) {
+printf("we in execute\n");
 
+int i = isComment(args);
 
-  if (isComment(args) == 1) 
+  if (i == 0) {
+    printf("out of iscomment\n");
+  
+  if (numTokens == 0){
+    printf("No arguments found!\n");
+    return 1;
+  }
+  if (args[0][0] == '#') 
     return isComment(args);
   
-  else if (args[0] == 'cd') 
+  else if (strcmp(args[0], "cd") == 0) 
     return changeDir(args);
    
-  else if (args[0] == 'lv') 
+  else if (strcmp(args[0],"lv") == 0) 
     return listVar(args);
   
-  else if (args[0] == '!') 
+  else if (args[0][0] == '!') 
     return execute_program(args);
     
-  else if (args[0] == 'unset'){
+  else if (strcmp(args[0], "unset") == 0)
     return unsetVar(args);
-  }
  
-  else if (numTokens == 3 && args[1] == '=')
+  else if (numTokens == 3 && *args[1] == '='){
+    printf("going to setvar\n");
     return setVar(args);
-
-  else if (args[0] == 'quit' || args[0] == '^D')
-    return exit(args);
-
-  else
-   setVar(args);
- 
-
+  }
+  else if ((strcmp(args[0], "quit")) == 0)
+    return exit_shell();
 
 }
+else {
+  printf("returning 1\n");
+  return 1;
+}
 
+}
 
 void shell_loop(void){
 
@@ -346,12 +426,21 @@ void shell_loop(void){
 
   do {
     printf("%s",varArray[2].value);
+    //added input = 
     Fgets(input, MAX, stdin);
+    if (*input == '\0')
+      x = exit_shell();
+    else {
     args = shell_scanner(input);
+    printf("calling execute\n");
     x = shell_execute(args);
+    printf("after execute\n");
+    }
+    //if (input != NULL && args != NULL)
+    //free(input);
 
-    free(input);
-    free(args);
+    //free(args);
+    
   } while(x);
   
 }
@@ -365,8 +454,6 @@ int main(int argc, char **argv) {
   varArray[2].name = "PS";
   varArray[2].value = ">> ";
   
-  
- 
   shell_loop();
 
 }
